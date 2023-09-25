461bc968ac0e13aab8db81e98a035b25142d8d9d
Nayid4 <nayid2004@gmail.com>
2023-09-25 15:14:16 -0500

    Creacion del backend de la tienda

---
+++ src/test/java/com/jnproduct/BackendTienda/BackendTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnproduct.BackendTienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
